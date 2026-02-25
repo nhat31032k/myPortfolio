0ed9af2236f4186156915edf94ba4b90336b6691
nhat3103 <nhat.nhq@intelin.vn>
2026-02-25 17:48:00 +0700

    add down load cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ import FallingStars from "./ui/falling-stars";
 import PortfolioPage from "./PortfolioPage";
 import SparklesText from "./ui/sparkles-text";
 import { FlipWords } from "./ui/flip-words";
+import CV from "../assets/NguyenHoangQuangNhat_CV.pdf";
 
 // AnimatedGrid Component
 const AnimatedGrid = () => {
@@ -164,7 +165,8 @@ const profile = {
 
                 {/* Contact Button */}
                 <a
-                  href="#"
+                  href={CV}
+                  download="NguyenHoangQuangNhat_CV.pdf"
                   className="group relative inline-flex items-center justify-center gap-3 p-0.5 rounded-xl bg-gradient-to-r from-gray-800 to-gray-700 transition-all duration-300 hover:scale-105 hover:shadow-[0_0_2rem_-0.5rem_#60A5FA]"
                 >
                   <span className="block w-full px-6 sm:px-8 py-3 sm:py-4 rounded-[11px] bg-gray-900 border border-gray-700/50 transition-all duration-300 group-hover:bg-gradient-to-r group-hover:from-gray-800 group-hover:to-gray-700">
